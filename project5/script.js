4ac2a62a5f92eb992c986705105266515d504e68
apacicco <apacicco@email.sc.edu>
2023-10-27 11:08:06 -0400

    added to json

--- project5/script.js
+++ project5/script.js
@@ -1,89 +1,84 @@
+const getMovies = async () => {
+    const url = "https://apacicco.github.io/project5/jewelry.json";
 
-const getItem = async() =>{
-    const url = "https://portiaportia.github.io/json/shoes.json";
-    try {
-        const response = await fetch (url);
+
+    try{
+        const response = await fetch(url);
         return await response.json();
-
-
-    } catch (error){
+    }catch(error)
+    {
         console.log(error);
     }
-
-}
-
-const showItem = async() =>{
-
-    //doc.getElementById("my-form").onsubmit = ...
-    let items = await getItem();
-    let homepg = document.getElementById("home-page");
-
-    items.forEach(item => {
-        homepg.append(getItemSection(item));
-    })
-}
-
-const getItemSection = (item) =>{
-    const section = document.createElement("section");
-    const h3 = document.createElement("h3");
-    section.append(h3);
-    h3.innerHTML = item.name;
-
-    const brandP = document.createElement("p");
-    section.append(brandP);
-    brandP.innerHTML = item.brand;
+};
 
 
-    const ul = document.createElement("ul");
-    section.append(ul);
+const showMovies =async()=> {
+    let movies = await getMovies();
+
+    let moviesSection = document.getElementById("movies-section");
     
-    for (let i in item.reviews){
-       // console.log(item.reviews[i]);
+    movies.forEach(movie => {
+        //movie is 1 movie
+        moviesSection.append(getMovieItem(movie));
+    });
+};
 
-       const li = document.createElement("li");
-       ul.append(li);
-       li.innerHTMl = item.reviews[i];
-    }
+const getMovieItem= (movie)=>{
+    let section = document.createElement("section");
+    let section2 = document.createElement("section");
+    let section3 = document.createElement("section");
+    let h3 = document.createElement("h3");
+    h3.innerText = movie.name;
+    
+    
+    section.append(section2);
+    section.append(section3);
+    
+    section.classList.add("section");
+    section2.classList.add("img-flexing");
+    
+    section3.classList.add("p-flex");
+    
+    /*
+    */
+
+    section3.append(h3);
+    let ul = document.createElement("ul");
+    section3.append(ul);
+   // ul.append(getLi(movie.director));
+    ul.append(getLi(`Category: ${movie.category}`));
+    ul.append(getLi(`Materials: ${movie.materials}`));
+    ul.append(getLi(`Price: ${movie.price}`));
+   // ul.append(getLi(`Description: ${movie.description}`));
+    
+
+   let img = document.createElement("img");
+   img.src = "https://apacicco.github.io/json/" + movie.img;
+   img.classList.add("img-sizer");
+   section2.append(img);
+
+    
 
 
+
+   // section.append(movie.img);
+    
+    
+    
+    
     return section;
 }
 
-window.onload =()=> showItem();
+const getLi = data => {
+    const li = document.createElement("li");
+    li.textContent = data;
 
-
-const getBreweires =async ()=> {
-    try {
-        return await fetch("https://api.openbrewerydb.org/breweries").json;
-    } catch (error){
-        console.log(error);
-    }
+    return li;
 };
 
-const showBreweries = async () => {
-    let breweries = await getBreweires();//beautiful json
-    let breweriesList = document.getElementById("some-section-in-html");
-
-    breweries.forEach((brewery)=>{
-        const section = document.createElement("section");
-        breweriesList.append(section);
-        section.classList.add("brewery");
 
 
-        const a =document.createElement("a");
-        a.href = brewery.website_url;
-        section.append(a);
 
-        const h3 = document.createElement("h3");
-        a.append(h3);
-        h3.innetHTML = brewery.name;
+ 
 
-        const p = document.createElement("p");
-        a.append(p);
-        p.innerHTML = `${brewer.brewery_type} brewery`;
-
-        const iframe = document.createElement("iframe");
-        iframe.src = "";
-    })
-
-};
+window.onload =() => showMovies();
